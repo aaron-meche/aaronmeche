9ba1ea4cf94e76f3ec4e8ad67301f143cb85f94a
Aaron Meche <aaronmeche18@icloud.com>
2026-02-08 16:22:30 -0600

    stripped finance transfer, skeleton website, added error page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import '$lib/main.css';
+	import favicon from '$lib/assets/icon.png';
+	import { resolve } from '$app/paths';
+	import { db } from '$lib/data'
+	let { children } = $props();
+
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Aaron Meche</title>
+	<script src="https://kit.fontawesome.com/5cf062dc93.js" crossorigin="anonymous"></script>
+</svelte:head>
+
+<div class="page">
+	{@render children()}
+</div>
+
+<style>
+	
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<!-- created by Aaron Meche -->
+<script>
+    
+</script>
+
+<!--  -->
+
+<div class="page">
+    This is the main page.
+</div>
+
+<!--  -->
+
+<style>
+    .page{
+        padding: 2rem;
+    }
+
+</style>
